f61123dafab054e1153764b180be70e7d56c554c
daveeedu <daveeedu@gmail.com>
2022-04-20 00:38:11 +0100

    New updates

--- _printf.c
+++ _printf.c
@@ -10,43 +10,41 @@
 int _printf(const char *format, ...)
 {
 	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	va_list args;
+	char *buf;
+	int (*f)(va_list);
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+	if (format == NULL)
 		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+	va_start(args, format);
+	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
+			i++;
+			if (format[i] == '\0')
+				break;
+			if (format[i] == '%')
+			{
+				_putchar('%');
+				i++;
+				continue;
 			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
+			f = get_func(format[i]);
+			if (f == NULL)
+				return (-1);
+			len = f(args);
+			i++;
 		}
 		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		{
+			_putchar(format[i]);
+			i++;
+			len++;
+		}
 	}
 	va_end(args);
-	return (count);
+	return (len);
 }
+
+
